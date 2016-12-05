d119bd124e9165c8520c286f34f51c4034521ed9
xsm <xieming1601@163.com>
2016-12-05 15:56:37 +0800

    init

---
+++ SwiftLearning/SwiftLearning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLearning
+//
+//  Created by xiesiming on 16/12/5.
+//  Copyright © 2016年 xiesiming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
